34851b6ba7fd7ea8908c9bf25b375a8c220c2e0d
Paul Aston <odders17@gmail.com>
2021-10-11 10:21:15 +0100

    use express and added connection to mongodb

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
